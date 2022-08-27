a01e8375819bdca11b4eb354494f89d0bffc7567
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-08-27 13:47:17 +0200

    Add border to image background

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,14 +11,14 @@ const Home: NextPage = () => {
         <p className='mb-3 font-normal text-gray-700'>
           File should be Jpeg, Png...
         </p>
-        <div className='mt-10 mb-4 grid gap-10 content-center w-80 h-56 bg-slate-100'>
+        <div className='rounded-lg border-2 border-dashed border-blue-200 mt-10 grid gap-10 content-center w-80 h-56 bg-slate-100'>
           <Image src={'/image.svg'} width={114} height={88} />
           <p>Drag &#038; drop your image here</p>
         </div>
-        <p className='mb-8 font-normal text-gray-700'>Or</p>
+        <p className='my-6 font-normal text-gray-700'>Or</p>
         <a
           href='#'
-          className='inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300'
+          className='inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300'
         >
           Choose a file
         </a>
