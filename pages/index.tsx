0097f7933f33a7744262d7738dc46d3443db16f6
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-08-27 13:36:20 +0200

    Add background to card image

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,18 @@ import Image from 'next/image';
 const Home: NextPage = () => {
   return (
     <div className='grid h-screen place-items-center text-center'>
-      <div className='p-6 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md'>
+      <div className='p-8 max-w-sm bg-white rounded-lg border border-gray-200 shadow-md'>
         <h2 className='mb-2 text-2xl font-bold tracking-tight text-gray-900'>
           Upload your image
         </h2>
         <p className='mb-3 font-normal text-gray-700'>
           File should be Jpeg, Png...
         </p>
-        <Image src={'/image.svg'} width={114} height={88} />
-        <p className='mb-3 font-normal text-gray-700'>Or</p>
+        <div className='mt-10 mb-4 grid gap-10 content-center w-80 h-56 bg-slate-100'>
+          <Image src={'/image.svg'} width={114} height={88} />
+          <p>Drag &#038; drop your image here</p>
+        </div>
+        <p className='mb-8 font-normal text-gray-700'>Or</p>
         <a
           href='#'
           className='inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300'
