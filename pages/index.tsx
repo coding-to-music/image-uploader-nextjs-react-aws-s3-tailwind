c86022d67f041d3b3e31fb3beafd1854e7ede23e
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-08-27 16:51:33 +0200

    Show dragged image in card

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { useDropzone } from 'react-dropzone';
 import type { NextPage } from 'next';
 import Image from 'next/image';
 
+interface FilesProps {
+  name: string;
+  preview: string;
+}
+
 const Home: NextPage = () => {
+  const [files, setFiles] = useState<FilesProps[]>([]);
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    accept: {
+      'image/*': [],
+    },
+    onDrop: acceptedFiles => {
+      setFiles(
+        acceptedFiles.map(file =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        )
+      );
+    },
+  });
+
+  const thumbs = files.map(file => (
+    <div key={file.name}>
+      <img
+        src={file.preview}
+        // Revoke data uri after image is loaded
+        onLoad={() => {
+          URL.revokeObjectURL(file.preview);
+        }}
+      />
+    </div>
+  ));
+
+  useEffect(() => {
+    // Revoke the data uris to avoid memory leaks, will run on unmount
+    return () => files.forEach(file => URL.revokeObjectURL(file.preview));
+  }, []);
+
   return (
     <div className='grid h-screen place-items-center'>
       <div className='p-8 max-w-sm text-center bg-white rounded-lg border border-gray-200 shadow-md'>
@@ -11,12 +52,21 @@ const Home: NextPage = () => {
         <p className='mb-3 font-light text-sm text-gray-500'>
           File should be Jpeg, Png,...
         </p>
-        <div className='rounded-lg border-2 border-dashed border-blue-200 mt-10 grid gap-10 content-center w-80 h-56 bg-slate-100'>
+        <div
+          {...getRootProps()}
+          className='rounded-lg border-2 border-dashed border-blue-200 mt-10 grid gap-10 content-center w-80 h-56 bg-slate-100'
+        >
+          <input {...getInputProps()} />
           <Image src={'/image.svg'} width={114} height={88} />
-          <p className='text-gray-400 font-light'>
-            Drag &#038; Drop your image here
-          </p>
+          {isDragActive ? (
+            <p className='text-gray-400 font-light'>Drop the files here ...</p>
+          ) : (
+            <p className='text-gray-400 font-light'>
+              Drag &#038; Drop your image here
+            </p>
+          )}
         </div>
+        {thumbs}
         <p className='my-6 font-light text-gray-500'>Or</p>
         <a
           href='#'
