bfdfa053108a9355c3b74b624fcf7f345171a2b4
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-08-30 14:49:04 +0200

    Change btn text when url copied to clipboard for 1s

--- components/UrlBar.tsx
+++ components/UrlBar.tsx
@@ -1,7 +1,8 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 
 const UrlBar = () => {
   const inputRef = useRef<HTMLInputElement>(null);
+  const [copied, setCopied] = useState(false);
 
   return (
     <div className='flex p-1 mt-10 bg-gray-50 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full'>
@@ -12,13 +13,18 @@ const UrlBar = () => {
       />
       <button
         onClick={() => {
-          if (inputRef.current?.value !== undefined) {
+          if (
+            inputRef.current?.value !== undefined &&
+            inputRef.current?.value.trim() !== ''
+          ) {
             navigator.clipboard.writeText(inputRef.current?.value);
+            setCopied(true);
+            setTimeout(() => setCopied(false), 1000);
           }
         }}
         className='inline-flex items-center py-2 px-5 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none'
       >
-        Copy
+        {copied ? 'Copied' : 'Copy'}
       </button>
     </div>
   );
