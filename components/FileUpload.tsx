ae6e44ae723fa4458475c346c45454070c77c709
Jascha-Biederstedt <j.biederstedt@gmx.de>
2022-08-30 15:19:00 +0200

    Refactor to FileUpload component

---
+++ components/FileUpload.tsx
@@ -0,0 +1,46 @@
+import Image from 'next/image';
+import { DropzoneInputProps, DropzoneRootProps } from 'react-dropzone';
+
+interface FileUploadProps {
+  getRootProps: <T extends DropzoneRootProps>(props?: T) => T;
+  getInputProps: <T extends DropzoneInputProps>(props?: T) => T;
+  open: () => void;
+  isDragActive: boolean;
+}
+
+const FileUpload: React.FC<FileUploadProps> = ({
+  getRootProps,
+  getInputProps,
+  open,
+  isDragActive,
+}) => {
+  return (
+    <>
+      <div
+        {...getRootProps()}
+        className='w-80 h-56 rounded-lg border-2 border-dashed border-blue-200 mt-10 grid gap-10 content-center bg-slate-100'
+      >
+        <input {...getInputProps()} />
+
+        <Image src={'/image.svg'} width={114} height={88} />
+        {isDragActive ? (
+          <p className='text-gray-400 font-light'>Drop the files here ...</p>
+        ) : (
+          <p className='text-gray-400 font-light'>
+            Drag &#038; Drop your image here
+          </p>
+        )}
+      </div>
+
+      <p className='my-6 font-light text-gray-500'>Or</p>
+      <button
+        onClick={open}
+        className='inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none'
+      >
+        Choose a file
+      </button>
+    </>
+  );
+};
+
+export default FileUpload;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
 import type { NextPage } from 'next';
-import Image from 'next/image';
 
 import CheckIcon from 'components/CheckIcon';
 import UploadBar from 'components/UploadBar';
 import CardHeader from 'components/CardHeader';
 import UrlBar from 'components/UrlBar';
+import FileUpload from 'components/FileUpload';
 
 export interface FilesProps {
   name: string;
@@ -69,33 +69,12 @@ const Home: NextPage = () => {
           {files.length !== 0 && <UrlBar />}
 
           {files.length === 0 && (
-            <>
-              <div
-                {...getRootProps()}
-                className='w-80 h-56 rounded-lg border-2 border-dashed border-blue-200 mt-10 grid gap-10 content-center bg-slate-100'
-              >
-                <input {...getInputProps()} />
-
-                <Image src={'/image.svg'} width={114} height={88} />
-                {isDragActive ? (
-                  <p className='text-gray-400 font-light'>
-                    Drop the files here ...
-                  </p>
-                ) : (
-                  <p className='text-gray-400 font-light'>
-                    Drag &#038; Drop your image here
-                  </p>
-                )}
-              </div>
-
-              <p className='my-6 font-light text-gray-500'>Or</p>
-              <button
-                onClick={open}
-                className='inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none'
-              >
-                Choose a file
-              </button>
-            </>
+            <FileUpload
+              getRootProps={getRootProps}
+              getInputProps={getInputProps}
+              open={open}
+              isDragActive={isDragActive}
+            />
           )}
         </div>
       )}
